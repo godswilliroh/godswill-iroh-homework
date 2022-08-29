b9e9fabe8382ab14d0be18b7532509e8d3464953
godswilliroh <112190453+godswilliroh@users.noreply.github.com>
2022-08-29 12:03:13 -0500

    Add files via upload

---
+++ hw_1_/hw_1_1.py
@@ -0,0 +1,6 @@
+import datetime
+curr = datetime.datetime.now()
+print("Current date:")
+print(curr.strftime("%Y-%m-%d"))
+print("Current time:")
+print(curr.strftime("%H:%M:%S"))
---
+++ hw_1_/hw_1_2.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input('What is the radius of the circle: '))
+area = math.pi * (radius^2)
+print(f'The area of this circle is {area}cm squared')
